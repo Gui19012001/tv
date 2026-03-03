a4649b7c94faf48d32fbfa97fdad58dc02f2dc06
Gui19012001 <guilherme.cruz@iberoequipamentos.com.br>
2026-03-03 20:35:16 -0300

    Update tv.py

--- tv.py
+++ tv.py
@@ -7,6 +7,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import streamlit.components.v1 as components
+import inspect
 from pathlib import Path
 
 # ==============================
